e80ef69e7b4b134728f02b88a729d929ca94e018
rlwjd31 <rlwjd31@naver.com>
2025-02-08 22:51:09 +0900

    🚀 [Feat]: UI chart content & apply banner to chart page
    chart content로서 들어갈 list에 대한 item을 구현했으며, Swiper Banner를 chart page에 적용

--- src/components/pages/Chart.tsx
+++ src/components/pages/Chart.tsx
@@ -1,42 +1,64 @@
-import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-import { bannerInfo } from "../../contants/banner";
-import { useRef } from "react";
+import Card from "../atoms/Card";
+import Banner from "../templates/banner/Banner";
 
-import "./banner.style.css";
-
-export default function Chart() {
-  const swiperRef = useRef<SwiperClass>(null);
-
+function CharBodySection() {
   return (
     <>
-      <Swiper
-        modules={[Pagination]}
-        className="flex flex-row w-full items-center justify-between font-bold h-72 transition-all duration-200 ease-out"
-        slidesPerView={1}
-        pagination={{
-          clickable: true,
-        }}
-        centeredSlides
-        grabCursor
-        loop
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
-      >
-        {bannerInfo.map((banner) => (
-          <SwiperSlide
-            className="w-full flex items-center justify-center size-full transition-all duration-200 ease-out pb-10"
-            key={banner.id}
-          >
-            <a href={banner.href} target="_blank">
-              <img
-                className="size-full object-fill rounded-2xl"
-                src={banner.imgSrc}
-              />
-            </a>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <div className="flex flex-1 size-full bg-neutral-200">something</div>
+      <h2 className="text-neutral-800 font-bold text-2xl mt-2 mb-4">
+        world chart
+      </h2>
+      <div className="flex justify-between pl-[46px] text-center text-sm font-medium text-neutral-600 mb-4">
+        <p className="w-[27.6rem] text-left">Album Info</p>
+        <div className="flex flex-1 gap-8 justify-between max-w-[240px]">
+          <p className="min-w-24">Sales Amount</p>
+          <div className="min-w-[7rem] justify-center">
+            <p>Physical record</p>
+            <p>index</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default function Chart() {
+  return (
+    <>
+      <Banner />
+      <div className="flex flex-col flex-1 size-full mt-8">
+        <CharBodySection />
+        <section className="flex flex-col h-full">
+          <Card className="text-neutral-900">
+            <div className="flex items-start">
+              <div className="flex translate-y-6 items-center text-lg font-bold">
+                1
+              </div>
+              <div className="flex mx-4 size-20 rounded-md overflow-hidden shrink-0">
+                <img
+                  className="size-full object-cover"
+                  src="https://resource.hanteochart.io/album/900540088_s150.jpg?now=1738969210404"
+                  alt="image1"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-1">
+              <div className="flex flex-col justify-center gap-1">
+                <p className="text-md break-all">
+                  Caligo Pt.1: 미니앨범 3집, POCAALBUM Ver.
+                </p>
+                <p className="font-bold uppercase text-neutral-500">PLAVE</p>
+              </div>
+            </div>
+            <div className="flex max-w-[248px] flex-1 justify-between gap-[38px] text-md font-semibold">
+              <p className="flex items-center justify-end min-w-24">96,512</p>
+              <p className="flex items-center justify-end min-w-[7rem]">
+                63,249.30
+              </p>
+            </div>
+          </Card>
+        </section>
+      </div>
     </>
   );
 }
