4e818c6915f4b1c22c4643121db1041c38c76343
rlwjd31 <rlwjd31@naver.com>
2025-02-08 22:49:58 +0900

    🚀 [Feat]: Implement custom swiper pagination bullets
    react-slick의 bullets의 Style을 커스터마이징하기 위해 현재 위치한 slider의 index를 통해 원하는 pagination bullet 스타일을 구현

--- src/components/templates/banner/Banner.tsx
+++ src/components/templates/banner/Banner.tsx
@@ -6,20 +6,63 @@ import "slick-carousel/slick/slick-theme.css";
 import { bannerInfo } from "../../../contants/banner";
 import { Settings } from "react-slick";
 import "./banner.style.css";
+import { useRef, useState } from "react";
+
+type PaginationBulletsProps = {
+  length: number;
+  currentIndex: number;
+  callback: (index: number) => void;
+  className?: string;
+};
+
+function PaginationBullets({
+  length,
+  callback,
+  currentIndex,
+  className = "",
+}: PaginationBulletsProps) {
+  return (
+    <div
+      className={`flex w-full py-2 gap-2 items-center justify-center z-50 mt-2 ${className}`}
+    >
+      {Array(length)
+        .fill(0)
+        .map((_, index) => (
+          <span
+            className={`size-2 rounded-full cursor-pointer ${
+              currentIndex === index ? "bg-[#FC5BA8]" : "bg-neutral-400"
+            }`}
+            key={index}
+            onClick={() => callback(index)}
+          />
+        ))}
+    </div>
+  );
+}
 
 export default function Banner() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const sliderRef = useRef<Slider>(null);
+
   const settings: Settings = {
-    dots: true,
+    // dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    afterChange: (currentIndex) => setCurrentIndex(currentIndex),
+  };
+
+  const onClickPaginationBullet = (index: number) => {
+    if (sliderRef.current) {
+      sliderRef.current.slickGoTo(index);
+    }
   };
 
   return (
     <div className="flex flex-col size-full max-h-64">
-      <Slider {...settings}>
+      <Slider {...settings} ref={sliderRef}>
         {bannerInfo.map((banner) => (
           <div key={banner.id}>
             <a href={banner.href} target="_blank">
@@ -28,6 +71,11 @@ export default function Banner() {
           </div>
         ))}
       </Slider>
+      <PaginationBullets
+        length={bannerInfo.length}
+        callback={onClickPaginationBullet}
+        currentIndex={currentIndex}
+      />
     </div>
   );
 }
