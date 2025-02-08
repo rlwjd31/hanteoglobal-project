b8ede6e3e6ba370d6861ae930769491f2767f535
rlwjd31 <rlwjd31@naver.com>
2025-02-08 11:34:31 +0900

    🚀 [Feat]: Add Footer
    Footer 구현

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "./templates/Header";
+import Footer from "./templates/Footer";
 
 export default function RootLayout() {
   return (
     <div className="w-dvw h-dvh px-2">
-      <div className="relative size-full max-w-[768px] flex flex-col bg-orange-100 min-w-[425px] min-h-[900px] mx-auto">
+      <div className="relative size-full max-w-[768px] flex flex-col bg-white min-w-[425px] min-h-[900px] mx-auto">
         <Header />
-        <main className="mt-(--height-header)">
+        <main className="flex flex-col items-center justify-center size-full bg-green-200 mt-(--height-header)">
           <Outlet />
         </main>
-        <footer>footer section</footer>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/templates/Footer.tsx
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-between items-center p-8 bg-neutral-800">
+      <div className="flex flex-col gap-8 flex-1">
+        <div className="flex gap-4 text-neutral-500 font-bold">
+          <a href="http://www.hanteoglobal.com/" target="_blank">
+            Company Introduction
+          </a>
+          <a
+            href="https://view.officeapps.live.com/op/view.aspx?src=https://resource.hanteochart.io/qna/%ED%95%9C%ED%84%B0%EC%B0%A8%ED%8A%B8_%EB%AC%B8%EC%9D%98%EC%8B%A0%EC%B2%AD%EC%84%9C.docx"
+            target="_blank"
+          >
+            Partnership
+          </a>
+        </div>
+        <div className="font-normal text-neutral-600">
+          <div>Copyright ⓒ HANTEO GLOBAL, Inc. All Rights Reserved</div>
+        </div>
+      </div>
+      <div>
+        <div className="w-32 h-20">
+          <img
+            className="img"
+            src="https://www.hanteochart.com/static/media/copyright_logo_en.d6b2417c3c3352031e33.png"
+            alt="저작권 OK 로고"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
