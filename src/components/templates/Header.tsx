af029125d5fb23018c6254e76b63ab71799c95ee
rlwjd31 <rlwjd31@naver.com>
2025-02-08 10:59:52 +0900

    🚀 [Feat]: Add Swiper to header
    header에 swiper기능을 추가
    
    - 좌우 터치 스크롤 기능
    - 좌우 무한 터치 스크롤 기능
    - nav 클릭시 navigation 기능

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,21 +1,44 @@
 import { NavLink } from "react-router-dom";
 import { routeInfo } from "../../contants/route";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper-bundle.css";
+import { useRef } from "react";
 
 export default function Header() {
+  const swiperRef = useRef<SwiperClass>(null);
+
   return (
     <header className="absolute w-full top-0 left-0 bg-header h-header">
-      <nav className="flex flex-row h-full items-center justify-between font-bold">
-        {Object.values(routeInfo).map((route) => (
-          <NavLink
-            key={route.id}
-            to={route.path}
-            className={({ isActive }) =>
-              isActive ? "text-white" : "text-black"
-            }
-          >
-            <li className="list-none">{route.content}</li>
-          </NavLink>
-        ))}
+      <nav className="h-full">
+        <Swiper
+          className="flex flex-row h-full items-center justify-between font-bold"
+          initialSlide={0}
+          slidesPerView={Object.keys(routeInfo).length - 1 + 0.2}
+          spaceBetween={20}
+          grabCursor
+          freeMode
+          loop
+        >
+          {Object.values(routeInfo).map((route) => (
+            <SwiperSlide
+              className="h-full"
+              key={route.id}
+              onClick={() => swiperRef.current?.slideTo(1, 400)}
+            >
+              <NavLink
+                to={route.path}
+                className={({ isActive }) =>
+                  `h-full flex items-center justify-center cursor-grab ${
+                    isActive ? "text-white" : "text-black"
+                  }`
+                }
+              >
+                <li className="list-none">{route.content}</li>
+              </NavLink>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </nav>
     </header>
   );
