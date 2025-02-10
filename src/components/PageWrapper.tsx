4ab0745bb59cf5842d0904a9e40a9ba22a50dcd5
rlwjd31 <rlwjd31@naver.com>
2025-02-10 13:47:06 +0900

    🚀 [Feat]: Get main width to apply translate
    translate효과에 필요한 main width를 구하여 window가 resize되었을 때 적절하게 translate x가 되도록 구현

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,6 +1,12 @@
 import { useNavigate } from "react-router-dom";
-import { animate, motion, useMotionValue, useTransform } from "framer-motion";
-import { useState } from "react";
+import {
+  animate,
+  motion,
+  PanInfo,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useState } from "react";
 import { routeInfo } from "../contants/route";
 
 export default function PageWrapper({
@@ -12,24 +18,39 @@ export default function PageWrapper({
   nextPage: React.ReactNode;
   prevPage: React.ReactNode;
 }) {
+  const OFFSET = 300;
   const navigate = useNavigate();
   const x = useMotionValue(0);
-  const opacity = useTransform(
-    x,
-    [-window.innerWidth, 0, window.innerWidth],
-    [0, 1, 0]
-  );
+  const [mainWidth, setMainWidth] = useState(768);
+  useEffect(() => {
+    const mainElement = document.querySelector("main");
+    if (mainElement) {
+      const updateMainWidth = () =>
+        setMainWidth(mainElement.getBoundingClientRect().width);
+      updateMainWidth();
 
-  const handleDragEnd = (event: any, info: any) => {
-    if (info.offset.x > 300) {
+      window.addEventListener("resize", updateMainWidth);
+
+      return () => {
+        if (updateMainWidth)
+          window.removeEventListener("resize", updateMainWidth);
+      };
+    }
+  }, []);
+
+  const handleDragEnd = (
+    _event: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo
+  ) => {
+    if (info.offset.x > OFFSET) {
       // 오른쪽으로 드래그
-      animate(x, 768, {
+      animate(x, mainWidth, {
         duration: 0.4,
         onComplete: () => navigate(routeInfo.chargeShop.path), // navigation상 이전 페이지
       });
-    } else if (info.offset.x < -300) {
+    } else if (info.offset.x < -OFFSET) {
       // 왼쪽으로 드래그
-      animate(x, -768, {
+      animate(x, -mainWidth, {
         duration: 0.4,
         onComplete: () => navigate(routeInfo.whook.path), // navigation상 다음 페이지
       });
@@ -53,19 +74,20 @@ export default function PageWrapper({
 
       {/* 다음 페이지 */}
       <motion.div
-        className="absolute size-full bg-orange-100"
+        className="absolute size-full"
         style={{
-          x: useTransform(x, (value) => value + 768),
+          x: useTransform(x, (value) => value + mainWidth),
         }}
         initial={{ x: window.innerWidth }}
       >
         {nextPage}
       </motion.div>
 
+      {/* 이전 페이지 */}
       <motion.div
-        className="absolute size-full bg-red-400"
+        className="absolute size-full"
         style={{
-          x: useTransform(x, (value) => value - 768),
+          x: useTransform(x, (value) => value - mainWidth),
         }}
         initial={{ x: window.innerWidth }}
       >
