a8969195834cb97846d3d78cc073f805bf665d54
rlwjd31 <rlwjd31@naver.com>
2025-02-10 11:30:05 +0900

    🚀 [Feat]: Migrate initial fetching with react 19 use hook
    초기 데이터 fetching시 불편한 가독성과 복잡한 boilerplate를 개선하기 위해 use hook으로 migration 진행

--- src/components/organisms/chart/ChartBodySection.tsx
+++ src/components/organisms/chart/ChartBodySection.tsx
@@ -1,64 +1,38 @@
-import { useEffect, useRef, useState } from "react";
-import { CurationContentItemType } from "../../../types/curationContent.type";
+import { use, useRef, useState } from "react";
 import { fakeFetchChart } from "../../../utils/fakeFetchChart";
 import CurationChartItem from "../../molecules/CurationChartItem";
+import { CurationContentItemType } from "../../../types/curationContent.type";
 import CustomIntersectionObserver from "../../molecules/CustomIntersectionObserver";
 import { ClipLoader } from "react-spinners";
 
+const initialCurationChartContentPromise = fakeFetchChart({
+  pageParam: 1,
+  pageDataLength: 10,
+  delay: 500,
+});
+
 export default function ChartBodySection() {
+  const initialCurationChartContent = use(initialCurationChartContentPromise);
   const [curationChartContent, setCurationChartContent] = useState<{
     contents: CurationContentItemType[];
     pageParam: number;
-    pageDataLength: number;
     isLoading: boolean;
     isLastPage: boolean;
   }>({
-    contents: [],
-    pageParam: 1,
-    pageDataLength: 10,
+    contents: initialCurationChartContent.data,
+    pageParam: 2,
     isLoading: false,
     isLastPage: false,
   });
-  const [initialLoading, setInitialLoading] = useState(true);
-  const firstMount = useRef<boolean>(true);
   const intersectingCount = useRef<number>(0);
-
   const commonHeight = "h-12";
-
-  useEffect(() => {
-    if (firstMount.current) {
-      (async () => {
-        const result = await fakeFetchChart({
-          pageParam: curationChartContent.pageParam,
-          pageDataLength: curationChartContent.pageDataLength,
-          delay: 500,
-        });
-
-        setInitialLoading(false);
-
-        setCurationChartContent((prev) => {
-          return {
-            ...prev,
-            contents: [...result.data],
-            pageParam: prev.pageParam + 1,
-            isLastPage: result.isLastPage,
-          };
-        });
-      })();
-
-      return () => {
-        firstMount.current = false;
-      };
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const fetchMoreCurationChartData = async () => {
     setCurationChartContent((prev) => ({ ...prev, isLoading: true }));
 
     const result = await fakeFetchChart({
       pageParam: curationChartContent.pageParam,
-      pageDataLength: curationChartContent.pageDataLength,
+      pageDataLength: 10,
       delay: 500,
     });
 
@@ -75,16 +49,11 @@ export default function ChartBodySection() {
     });
   };
 
-  return initialLoading ? (
-    <div className="flex h-full justify-center items-center">
-      <ClipLoader key="ClipLoader" size={40} loading color="#643927" />
-    </div>
-  ) : (
-    <section className="flex flex-col h-full overflow-auto gap-y-4 px-2">
-      {(curationChartContent.contents ?? []) &&
-        curationChartContent.contents.map((content) => (
-          <CurationChartItem key={content.id} {...content} />
-        ))}
+  return (
+    <section className="flex flex-col h-full overflow-auto gap-y-4 px-2 pb-2">
+      {curationChartContent.contents.map((content) => (
+        <CurationChartItem key={content.id} {...content} />
+      ))}
       {!curationChartContent.isLastPage && !curationChartContent.isLoading && (
         <CustomIntersectionObserver
           callback={() => {
--- src/components/pages/Chart.tsx
+++ src/components/pages/Chart.tsx
@@ -1,7 +1,17 @@
+import { Suspense } from "react";
 import Container from "../atoms/Container";
-import ChartBodySection from "../organisms/chart/ChartBodySection";
+import ChartBodySection from "../organisms/chart/ChartBodySection.tsx";
 import ChartHeaderSection from "../organisms/chart/ChartHeaderSection";
 import Banner from "../templates/banner/Banner";
+import { ClipLoader } from "react-spinners";
+
+function Loading() {
+  return (
+    <div className="flex h-full justify-center items-center">
+      <ClipLoader key="ClipLoader" size={40} loading color="#643927" />
+    </div>
+  );
+}
 
 export default function Chart() {
   return (
@@ -9,7 +19,9 @@ export default function Chart() {
       <Container.FlexCol className="size-full">
         <Banner />
         <ChartHeaderSection />
-        <ChartBodySection />
+        <Suspense fallback={<Loading />}>
+          <ChartBodySection />
+        </Suspense>
       </Container.FlexCol>
     </>
   );
