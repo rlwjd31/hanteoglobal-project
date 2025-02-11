05c3ac12686ab589471900584afa945528ef4855
rlwjd31 <rlwjd31@naver.com>
2025-02-12 03:51:31 +0900

    🚑️ [Fix]: Navigation only touching edge of main container(@9c8e1aa)
    EDGE_WIDTH를 이용하여 main container의 양끝을 감지하고 isEdgeTouch를 이용하여 현재페이지에 해당하는 motion.div의 drag가능 여부를 설정하여 해결.
    이 때 처음 component mount시에는 무조건 drag의 가능 여부가 false이므로 edge영역에 포인터가 들어오는 순간 drag가 가능하게 설정하여 두 번클릭해야 page navigation이 가능한 조건을 제거

--- src/components/molecules/PageWrapper.tsx
+++ src/components/molecules/PageWrapper.tsx
@@ -6,7 +6,7 @@ import {
   useMotionValue,
   useTransform,
 } from "framer-motion";
-import { ReactNode, useLayoutEffect, useState } from "react";
+import { PointerEvent, ReactNode, useLayoutEffect, useState } from "react";
 
 export default function PageWrapper({
   currentPage,
@@ -22,68 +22,106 @@ export default function PageWrapper({
   prevPath: string;
 }) {
   const OFFSET = 200;
+  const EDGE_WIDTH = 50;
   const navigate = useNavigate();
   const x = useMotionValue(0);
   const location = useLocation();
-  const [mainWidth, setMainWidth] = useState(768);
+  const [mainSizeInfo, setMainSizeInfo] = useState({
+    width: 768,
+    left: 0,
+    right: 0,
+  });
+  const [isEdgeTouch, setIsEdgeTouch] = useState(false);
 
   useLayoutEffect(() => {
     const mainElement = document.querySelector("main");
     if (mainElement) {
-      const updateMainWidth = () =>
-        setMainWidth(mainElement.getBoundingClientRect().width);
-      updateMainWidth();
+      const updateMainSizeInfo = () => {
+        const { left, right, width } = mainElement.getBoundingClientRect();
+        setMainSizeInfo({
+          width,
+          left,
+          right,
+        });
+      };
+      updateMainSizeInfo();
 
-      window.addEventListener("resize", updateMainWidth);
+      window.addEventListener("resize", updateMainSizeInfo);
 
       return () => {
-        if (updateMainWidth)
-          window.removeEventListener("resize", updateMainWidth);
+        if (updateMainSizeInfo)
+          window.removeEventListener("resize", updateMainSizeInfo);
       };
     }
   }, []);
+
+  const handlePointerMove = (event: PointerEvent) => {
+    const isPointerPressed = event.pressure;
+    const touchX = event.clientX;
+    // 양끝 EDGE_WIDTH내부에서 터치 감지
+    const edgeCondition =
+      touchX - mainSizeInfo.left <= EDGE_WIDTH ||
+      mainSizeInfo.right - touchX <= EDGE_WIDTH;
+
+    // 맨처음 rendering시에는 isEdgeTouch가 false로 인해 drag가 안 되므로 두 번의 클릭이 존재해야 page navigation이 가능한 상태가 된다.
+    // 따라서, edge area에 들어오는 순간 touch가 되었다고 가정하고 isEdgeTouch를 true로 설정한다.
+    // 또한 edge area에서 벗어나면 isEdgeTouch를 false로 설정되는데 pointer가 눌러지고 있다면 isEdgeTouch를 true로 유지하여
+    // motion.div가 drag되도록 한다.
+    if (isPointerPressed || edgeCondition) {
+      setIsEdgeTouch(true)
+    }else {
+      setIsEdgeTouch(false)
+    }
+  };
 
   const handleDragEnd = async (
     _event: MouseEvent | TouchEvent | PointerEvent,
     info: PanInfo
   ) => {
-    if (info.offset.x > OFFSET) {
-      // 오른쪽으로 드래그
-      await animate(x, mainWidth, {
-        duration: 0.4,
-      });
+    if (isEdgeTouch) {
+      if (info.offset.x > OFFSET) {
+        // 오른쪽으로 드래그
+        await animate(x, mainSizeInfo.width, {
+          duration: 0.4,
+        });
 
-      navigate(prevPath);
-      requestAnimationFrame(() => {
-        x.set(0);
-      });
-    } else if (info.offset.x < -OFFSET) {
-      // 왼쪽으로 드래그
+        navigate(prevPath);
+        requestAnimationFrame(() => {
+          x.set(0);
+        });
+      } else if (info.offset.x < -OFFSET) {
+        // 왼쪽으로 드래그
 
-      await animate(x, -mainWidth, {
-        duration: 0.4,
-      });
+        await animate(x, -mainSizeInfo.width, {
+          duration: 0.4,
+        });
 
-      navigate(nextPath);
-      requestAnimationFrame(() => {
-        x.set(0);
-      });
-    } else {
-      await animate(x, 0, { duration: 0.4 }); // OFFSET만큼 이동하지 않았을 때 원래 위치로
+        navigate(nextPath);
+        requestAnimationFrame(() => {
+          x.set(0);
+        });
+      } else {
+        await animate(x, 0, { duration: 0.4 }); // OFFSET만큼 이동하지 않았을 때 원래 위치로
+      }
     }
+
+    setIsEdgeTouch(false);
   };
 
   return (
-    <div className="relative size-full grid grid-cols-1 grid-rows-1 overflow-hidden">
+    <div
+      className={`relative size-full grid grid-cols-1 grid-rows-1 overflow-hidden`}
+    >
       {/* 현재 페이지 */}
       <motion.div
         key={location.pathname}
         className="size-full bg-white z-10"
         style={{ x }}
-        drag="x"
+        drag={isEdgeTouch ? "x" : false}
         dragConstraints={{ left: 0, right: 0 }}
         onDragEnd={handleDragEnd}
         transition={{ duration: 1, ease: "easeOut" }}
+        onPointerMove={handlePointerMove}
       >
         {currentPage}
       </motion.div>
@@ -93,7 +131,7 @@ export default function PageWrapper({
         key={nextPath}
         className="absolute top-0 left-0 size-full"
         style={{
-          x: useTransform(x, (value) => value + mainWidth),
+          x: useTransform(x, (value) => value + mainSizeInfo.width),
         }}
       >
         {nextPage}
@@ -104,7 +142,7 @@ export default function PageWrapper({
         key={prevPath}
         className="absolute top-0 left-0 size-full"
         style={{
-          x: useTransform(x, (value) => value - mainWidth),
+          x: useTransform(x, (value) => value - mainSizeInfo.width),
         }}
       >
         {prevPage}
